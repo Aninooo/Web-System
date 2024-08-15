cb182dffa78b73e6b6cf78dddcc5059e8e4c2bae
Ani no <bryanlomerioanino@gmail.com>
2024-08-16 00:35:13 +0800

    first commit

---
+++ js/sigin.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('signin').addEventListener('click', function() {
+        alert('Sign In button clicked');
+    });
+});
